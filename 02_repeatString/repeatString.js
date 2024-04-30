7d213dc7f2b84c8f1c498a3736622d742e345290
IseSize <127502987+IseSize@users.noreply.github.com>
2024-04-30 22:26:49 +0200

    feat: solve 2nd exercise - 2 solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    // Solution2 -- as requested in #Hints in README.md 
+    let repeatedString = "";
+    if(num < 0) {
+        return "ERROR";
+    }
+    for(let i = 0; i < num; i++) {
+        repeatedString += string;
+    }
+    return repeatedString;
+
+
+    
+    // Solution1 -- .repeat() method
+    // if(num < 0) {
+    //     return "ERROR";
+    // }
+    // return string.repeat(num);
 };
 
 // Do not edit below this line
