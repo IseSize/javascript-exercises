fd662cc2901ee9eee2f6016f14c7fe438e02e866
IseSize <127502987+IseSize@users.noreply.github.com>
2024-05-01 19:36:02 +0200

    feat: solve exercice 4
    Two implementation, w/ filter() and splice()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,32 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayToUpdate, ...toRemove) {  
+    let updatedArray = arrayToUpdate.filter((elem) => {
+        for(const elemToRemove of toRemove) {
+            if(elem === elemToRemove) {
+                return false;
+            }
+        }
+        return true;
+    });
+    return updatedArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+    
+
+// First idea, but increase complexity 
+    // const removeFromArray = function(arrayToUpdate, ...toRemove) { 
+    //     for (const elemToRemove of toRemove) {
+    //         let arrayLength = arrayToUpdate.length;
+    //         for(let i = 0; i < arrayLength; i++ ){
+    //             if(arrayToUpdate[i] === elemToRemove) {
+    //                 arrayToUpdate.splice(i, 1);
+    //                  arrayLength = arrayToUpdate.length; //to not iterate over the new length
+    //                  i--; //neutralize index offset due to remove
+    //             }
+    //         }
+    //     }
+    //     return arrayToUpdate;
+    // };
