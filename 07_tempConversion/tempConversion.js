7f5e1c63455a66a51f492e04e26afe17ef3aec67
IseSize <127502987+IseSize@users.noreply.github.com>
2024-05-01 22:35:52 +0200

    feat: solve exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+	let tempCelcius = (tempFahrenheit - 32) * (5/9);
+    return Math.round(tempCelcius * 10) / 10; //round to 1 decimal
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelcius) {
+	let tempFahrenheit = (tempCelcius *(9/5)) + 32;
+	return Math.round(tempFahrenheit * 10) / 10; //round to 1 decimal
 };
 
 // Do not edit below this line
