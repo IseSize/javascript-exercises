b022b7d4bc39ec9164efebb4462983198855d764
IseSize <127502987+IseSize@users.noreply.github.com>
2024-05-01 21:07:05 +0200

    feat: solve exercice 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if(x < 0  || y < 0 || typeof(x) !== "number" || typeof(y) !== "number") {
+        return "ERROR";
+    }
+    let infNumber = x;
+    let supNumber = y;
+    if (x > y) { 
+        infNumber = y;
+        supNumber = x;
+    }
+    let sum = 0;
+    for(let i = infNumber; i <= supNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
