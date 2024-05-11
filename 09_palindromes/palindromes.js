a5b3137aee65e5a7e8006755dfdccc95f344578b
IseSize <127502987+IseSize@users.noreply.github.com>
2024-05-11 21:40:59 +0200

    feat: solve exo9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strFormated ="";
+    let removeChars = ["!", ",", ".", " "];
+    for (let char of str) {
+        if(!removeChars.includes(char)) {
+            strFormated += char.toLowerCase();
+        }
+    }
+    let reversedStr = strFormated.split('').reverse().join("");
+    if (strFormated === reversedStr) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
